f42d7665c226766ce8a0e6a720795f5f7863c371
kahenya-anita <anitakahenya1@gmail.com>
2020-11-13 15:55:18 +0300

    Validate Mpesa-stk

---
+++ swipe-pay.php
@@ -0,0 +1,175 @@
+<?php
+/**
+Plugin Name: SWype-pay
+Description: Add Github project information using shortcode.
+Version: 1.0
+Author: Anita Kahenya
+License: GPLv2 or later
+Text Domain: swypepay-api
+*/
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+/*
+SwypePay Keys & Variables Definition
+
+Your SwypePay Merchant Code
+*/
+$merchant_code = 'DEMO1';
+$consumer_key = 'NDBRSnR4ZE1TWmxsemlndWROdUIzMk9uV0xS';//Your SwypePay Live App Consumer Key Register a developer account on developer.swypepay.africa
+                  
+$consumer_secret = 'NDBlZ0FtSVFiakh1UExueTY3alNxZlVWSW5KNVdGblJFZW1NYUJsQ0lT';//Your SwypePay Merchant Live App Consumer Secret Use the secret from your developer account as well.
+
+// Generate youe secure credentials
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.api.swypepay.africa/v3/apis/credentials",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => array('consumer_key' => $consumer_key,'consumer_secret' => $consumer_secret,'merchant_code' => $merchant_code),
+));
+
+$response = curl_exec($curl);
+
+//Parse JSON response
+$response_obj = json_decode($response);
+curl_close($curl);
+
+//These parameters will be passed in your header for the next requests to SwypePay.
+//$credentials = $response_obj->data->credentials;
+//$global_key = $response_obj->data->{'Global-key'};
+//$server_url = $response_obj->data->{'Server-url'};
+
+$credentials = "MDgyOTE1MTk2MDc1NTk1NGFiZmM3MDE2ZjMxNGRlN2RjYzM1MjcyZmQ4OTRjMzFmZDIyNzdiODQwNWRhMWY3YzU2ZjY4MjJjZGEwMGNkZTMyNDc2ZTJkYmIxMjdjOGJkZWNlNTVlMDU0Y2MzY2FmODcyMWRkM2Q0Mjk1M2E0MTk=";
+$global_key = "eRaMidkYj4R1vrkglUhh";
+$server_url = "100.100.100.1";
+
+
+
+//Generate API token for your call
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.api.swypepay.africa/v3/apis/token",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => array('merchant_code' => $merchant_code,'credentials' => $credentials),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+//Parse your token validation response
+ $response_obj_token=json_decode($response);
+ 
+ //Get your token
+ 
+ $token = $response_obj_token->data->token;
+ 
+// Finally let's generate the iframe and load payment options. 
+
+//Begin by filling your form data, this data can be passed to the iframs from your checkout form
+
+//Form Data
+$request_id="ws_CO_040620200606088489";
+//$phone ="25471396806";//or could be $_POST['phone']; if you're collecting information from a form. :-) Simple huh?
+//$wallet ="1"; //Change to 1 to enable SwypePay e-wallet payments
+//$amount = "10"; //Amount you wish to charge your customer
+// $currency = "KES";//Don't change the currency. Only KES is currently supported. 
+// $merchantcode = "DEMO1"; //Your verified merchant code e.g. SAMPLECODE
+// $ecommerce = "1"; //1 to enable payment method; 0 to disable
+// $mpesa = "1";//1 to enable payment method; 0 to disable
+// $tkash = "1";//1 to enable payment method; 0 to disable
+// $visa = "1";//1 to enable payment method; 0 to disable
+// $mastercard = "1";//1 to enable payment method; 0 to disable
+// $american_express = "1";//1 to enable payment method; 0 to disable
+// $order_ref = "1";//Your unique order reference
+// $callback = "https://www.yourdomain.com";//We'll navigate to this URL after a successful payment
+$curl = curl_init();
+
+
+// curl_setopt_array($curl, array(
+//     CURLOPT_URL => "https://www.api.swypepay.africa/v3/apis/mpesa-stk",
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => "",
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => "POST",
+//     CURLOPT_POSTFIELDS => array('phone' => $phone,'amount' => $amount),
+//     CURLOPT_HTTPHEADER => array(
+//       "Global-key: $global_key",
+//       "Server-url: $server_url",
+//       "Access-Token: $token"
+//     ),
+//   ));
+  
+//   $response_from_api = curl_exec($curl);
+  
+//   curl_close($curl);
+  
+//   //Parse JSON response
+//   $response_obj = json_decode($response_from_api);
+//   //Get back iframe link
+//   $reference_code= $response_obj->data->reference_code;
+//   $phone= $response_obj->data->phone;
+
+//   <!-->
+  
+  // <!-- Display iframe on your website-->
+  // <php 
+//   echo $reference_code;
+  
+//   echo $phone;
+  
+//   > 
+ 
+ curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://www.api.swypepay.africa/v3/apis/validate-mpesa-stk",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => array('request_id' => $request_id),
+    CURLOPT_HTTPHEADER => array(
+      "Global-key: $global_key",
+      "Server-url: $server_url",
+      "Access-Token: $token"
+    ),
+  ));
+  
+  $validate_mpesa = curl_exec($curl);
+  
+  curl_close($curl);
+  
+  //Parse JSON response
+  $response_obj = json_decode($validate_mpesa);
+  //Get back iframe link
+  $reference_code= $response_obj->data->reference_code;
+ 
+
+  ?>
+  
+  <!-- Display iframe on your website-->
+  <?php 
+  echo $reference_code;
+  
+  
+  ?>
+ 
